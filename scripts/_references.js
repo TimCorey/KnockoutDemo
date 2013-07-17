b22ab7f7411ea76957c80dbe8aa98076f4e530e9
me@timothycorey.com <me@timothycorey.com>
2013-07-17 16:08:54 -0400

    initial commit

---
+++ scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="site.js" />
+/// <reference path="amplify.js" />
+/// <reference path="jquery-2.0.3.js" />
+/// <reference path="knockout-2.3.0.debug.js" />
+/// <reference path="knockout.mapping-latest.debug.js" />
+/// <reference path="toastr.js" />
+/// <reference path="bootstrap.js" />
---
+++ scripts/demo1.js
@@ -0,0 +1,26 @@
+﻿var myJS = (function (myJS, $) {
+
+    function vm() {
+        var self = this;
+
+        self.firstName = ko.observable('Tim');
+        self.lastName = 'Corey';
+        self.twitterHandle = '@IAmTimCorey';
+        self.emailAddress = 'me@timothycorey.com';
+
+        self.fullName = ko.computed(function () {
+            return self.firstName() + ' ' + self.lastName;
+        });
+
+        self.checkValues = function(){
+            toastr.info(self.firstName(), "First Name");
+            toastr.info(self.lastName, "Last Name");
+        };
+    };
+
+    $(function () {
+        ko.applyBindings(vm());
+    });
+
+    return myJS;
+}(window.myJS || {}, jQuery));
