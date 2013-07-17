540c1349989eea50e4087e4248306f55444693f5
Timothy Corey <me@timothycorey.com>
2013-07-17 18:44:00 -0400

    demo1 complete

---
+++ Demo1/complete/site.js
@@ -0,0 +1,29 @@
+﻿// The module pattern (with an exported namespace)
+var myJS = (function (myJS, $) {
+
+    // My ViewModel - this holds the data and functions surrounding the data
+    var vm = function() {
+        var self = this;
+
+        self.firstName = ko.observable('Tim');
+        self.lastName = ko.observable('Corey');
+        self.twitterHandle = '@IAmTimCorey';
+        self.emailAddress = 'me@timothycorey.com';
+
+        self.fullName = ko.computed(function () {
+            return self.firstName() + ' ' + self.lastName();
+        });
+
+        self.checkValues = function(){
+            toastr.info(self.firstName(), "First Name");
+            toastr.info(self.lastName(), "Last Name");
+        };
+    };
+
+    // The Document.Ready jQuery event that fires once the DOM is loaded
+    $(function () {
+        ko.applyBindings(vm());
+    });
+
+    return myJS;
+}(window.myJS || {}, jQuery));
---
+++ Demo1/initial/site.js
@@ -0,0 +1,16 @@
+﻿// The module pattern (with an exported namespace)
+var myJS = (function (myJS, $) {
+
+    // My ViewModel - this holds the data and functions surrounding the data
+    var vm = function() {
+        var self = this;
+
+    };
+
+    // The Document.Ready jQuery event that fires once the DOM is loaded
+    $(function () {
+        
+    });
+
+    return myJS;
+}(window.myJS || {}, jQuery));
